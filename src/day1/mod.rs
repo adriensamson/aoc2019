45591b5ba43f6f4679f377d9a7104ed181c3b762
Adrien Samson <adrien@adriensamson.fr>
2019-12-01 20:59:37 +0100

    day 1 step 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,3 +1,14 @@
-pub fn step1(input : &str) {
+use std::str::FromStr;
 
+pub fn step1(input : &str) {
+    let mut sum = 0;
+    for line in input.lines().filter(|s| s.len() > 0) {
+        let mass = u64::from_str(line).unwrap();
+        sum += get_fuel_for_module(mass);
+    }
+    println!("{}", sum);
+}
+
+fn get_fuel_for_module(mass : u64) -> u64 {
+    mass / 3 - 2
 }
