d77a9bbddcd48b174acfc37f683606a8b1668aa6
Adrien Samson <adrien@adriensamson.fr>
2019-12-01 20:51:47 +0100

    init

---
+++ src/day1/mod.rs
@@ -0,0 +1,3 @@
+pub fn step1(input : &str) {
+
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate regex;
+extern crate core;
+
+use std::env;
+use std::str::FromStr;
+use std::fs::read_to_string;
+
+mod day1;
+
+const DEFAULT_DAY : i32 = 23;
+const DEFAULT_STEP : i32 = 2;
+const DEFAULT_FILE : &str = "input";
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    let day = args.get(1). map(parse_int).unwrap_or(DEFAULT_DAY);
+    let step = args.get(2).map(parse_int).unwrap_or(DEFAULT_STEP);
+
+    let filename = format!("src/day{}/{}.txt", day, args.get(3).map(|s| s.as_ref()).unwrap_or(DEFAULT_FILE));
+    let input = &read_to_string(filename).unwrap();
+
+    match (day, step) {
+        (1, 1) => day1::step1(input),
+    }
+}
+
+
+fn parse_int(s : &String) -> i32 {
+        i32::from_str(s).unwrap()
+}
