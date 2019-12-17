50718b3f6396c6c5804e68acb80bbb3f6a6bba47
Adrien Samson <adrien@adriensamson.fr>
2019-12-17 14:01:18 +0100

    day 11 part 1

---
+++ src/day11/mod.rs
@@ -0,0 +1,133 @@
+use std::collections::HashMap;
+use crate::intcode::{IntCode, IntCodeIo};
+use std::cell::RefCell;
+
+pub fn step1(input : &str) {
+    let robot = RefCell::new(Robot::new());
+    let io = RobotIo::new(&robot);
+    let mut program = IntCode::from_str(input, io);
+    program.run();
+    println!("{}", robot.borrow().count_painted_panels());
+}
+
+pub fn step2(input : &str) {}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+
+    fn turn_left(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Right => Direction::Up,
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+        }
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, Copy, Clone)]
+struct Coord {
+    x : i64,
+    y : i64,
+}
+
+impl Coord {
+    fn move_in_direction(&mut self, direction : Direction) {
+        match direction {
+            Direction::Up => { self.y -= 1 },
+            Direction::Right => { self.x += 1 },
+            Direction::Down => { self.y += 1 },
+            Direction::Left => { self.x -= 1 },
+        }
+    }
+}
+
+struct Robot {
+    panels: HashMap<Coord, bool>,
+    position: Coord,
+    direction : Direction,
+}
+
+impl Robot {
+    fn new() -> Robot {
+        Robot {
+            panels: HashMap::new(),
+            position : Coord {x: 0, y: 0},
+            direction : Direction::Up,
+        }
+    }
+
+    fn count_painted_panels(&self) -> usize {
+        self.panels.len()
+    }
+
+    fn get_color(&self) -> bool {
+        *self.panels.get(&self.position).unwrap_or(&false)
+    }
+
+    fn set_color(&mut self, color : bool) {
+        self.panels.insert(self.position, color);
+    }
+
+    fn turn_and_move(&mut self, right : bool) {
+        if right {
+            self.direction = self.direction.turn_right();
+        } else {
+            self.direction = self.direction.turn_left();
+        }
+        self.position.move_in_direction(self.direction);
+    }
+}
+
+enum NextOutput {
+    Color,
+    Turn,
+}
+
+struct RobotIo<'a> {
+    robot : &'a RefCell<Robot>,
+    next_output : NextOutput,
+}
+
+impl<'a> RobotIo<'a> {
+    fn new(robot : &'a RefCell<Robot>) -> RobotIo<'a> {
+        RobotIo {
+            robot,
+            next_output: NextOutput::Color,
+        }
+    }
+}
+
+impl<'a> IntCodeIo for RobotIo<'a> {
+    fn input(&mut self) -> Option<i64> {
+        Some(if self.robot.borrow().get_color() { 1 } else { 0 })
+    }
+
+    fn output(&mut self, val: i64) {
+        match self.next_output {
+            NextOutput::Color => {
+                self.robot.borrow_mut().set_color(val == 1);
+                self.next_output = NextOutput::Turn;
+            },
+            NextOutput::Turn => {
+                self.robot.borrow_mut().turn_and_move(val == 1);
+                self.next_output = NextOutput::Color;
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,10 @@ mod day7;
 mod day8;
 mod day9;
 mod day10;
+mod day11;
 
-const DEFAULT_DAY : i32 = 10;
-const DEFAULT_STEP : i32 = 2;
+const DEFAULT_DAY : i32 = 11;
+const DEFAULT_STEP : i32 = 1;
 const DEFAULT_FILE : &str = "input";
 
 fn main() {
@@ -50,6 +51,8 @@ fn main() {
         (9, 2) => day9::step2(input),
         (10, 1) => day10::step1(input),
         (10, 2) => day10::step2(input),
+        (11, 1) => day11::step1(input),
+        (11, 2) => day11::step2(input),
         _ => println!("Unknown day or step"),
     }
 }
