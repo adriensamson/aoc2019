31f2910e64532f02978556990524a0a17e603488
Adrien Samson <adrien@adriensamson.fr>
2019-12-23 19:33:32 +0100

    optimize a little day 16 step 2

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -5,7 +5,7 @@ pub fn step1(input : &str) {
     for _ in 0..100 {
         signal = signal.apply_phase();
     }
-    println!("{}", signal.get_8_digits(0));
+    println!("{}", signal.get_8_digits());
 }
 pub fn step2(input : &str) {
     let mut real_input = String::new();
@@ -13,21 +13,32 @@ pub fn step2(input : &str) {
         real_input.push_str(input);
     }
     let mut signal = Signal::from_str(&real_input);
-    for _ in 0..100 {
+    let offset = usize::from_str(&input[0..7]).unwrap();
+    signal = signal.offset(offset);
+    for i in 0..100 {
+        println!("{}", i);
         signal = signal.apply_phase();
     }
-    let offset = usize::from_str(&input[0..7]).unwrap();
-    println!("{}", signal.get_8_digits(offset));
+
+    println!("{}", signal.get_8_digits());
 }
 
 struct Signal {
     digits : Vec<i64>,
+    offset : usize,
 }
 
 impl Signal {
     fn from_str(s : &str) -> Signal {
         let digits = s.chars().map(|c| i64::from_str(&c.to_string()).unwrap()).collect();
-        Signal {digits}
+        Signal {digits, offset: 0}
+    }
+
+    fn offset(&self, offset : usize) -> Signal {
+        Signal {
+            offset,
+            digits: self.digits[offset..].iter().map(|i| *i).collect(),
+        }
     }
 
     fn apply_phase(&self) -> Signal {
@@ -35,22 +46,19 @@ impl Signal {
         for n in 0..self.digits.len() {
             digits.push(self.get_new_digit(n));
         }
-        Signal { digits }
+        Signal { digits, offset: self.offset }
     }
 
     fn get_new_digit(&self, n : usize) -> i64 {
-        let n1 = n + 1;
+        let n1 = self.offset + n + 1;
         let base : Vec<i64> = vec![0, 1, 0, -1];
         let mut sum = 0;
         let mut i = n;
         while i < self.digits.len() {
-            let fact = base[((i + 1)/n1) % 4];
-            if fact == 0 {
-                i += n1;
-                continue;
-            }
-            sum += self.digits[i] * fact;
-            i += 1;
+            let fact = base[((self.offset + i + 1)/n1) % 4];
+            let s : i64 = self.digits[i..].iter().take(n1).sum();
+            sum += fact * s;
+            i += 2 * n1;
         }
         if sum > 0 {
             sum % 10
@@ -59,7 +67,7 @@ impl Signal {
         }
     }
 
-    fn get_8_digits(&self, offset : usize) -> String {
-        format!("{}{}{}{}{}{}{}{}", self.digits[offset + 0], self.digits[offset + 1], self.digits[offset + 2], self.digits[offset + 3], self.digits[offset + 4], self.digits[offset + 5], self.digits[offset + 6], self.digits[offset + 7])
+    fn get_8_digits(&self) -> String {
+        format!("{}{}{}{}{}{}{}{}", self.digits[0], self.digits[1], self.digits[2], self.digits[3], self.digits[4], self.digits[5], self.digits[6], self.digits[7])
     }
 }
