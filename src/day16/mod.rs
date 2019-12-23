a97090e312050841701bc33f5a0db098fbb94bd6
Adrien Samson <adrien@adriensamson.fr>
2019-12-23 18:48:09 +0100

    optimize day 16 step 1

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -5,7 +5,7 @@ pub fn step1(input : &str) {
     for _ in 0..100 {
         signal = signal.apply_phase();
     }
-    println!("{}{}{}{}{}{}{}{}", signal.digits[0], signal.digits[1], signal.digits[2], signal.digits[3], signal.digits[4], signal.digits[5], signal.digits[6], signal.digits[7]);
+    println!("{}", signal.get_8_digits(0));
 }
 pub fn step2(input : &str) {
     let mut real_input = String::new();
@@ -17,7 +17,7 @@ pub fn step2(input : &str) {
         signal = signal.apply_phase();
     }
     let offset = usize::from_str(&input[0..7]).unwrap();
-    println!("{}{}{}{}{}{}{}{}", signal.digits[offset], signal.digits[offset+1], signal.digits[offset+2], signal.digits[offset+3], signal.digits[offset+4], signal.digits[offset+5], signal.digits[offset+6], signal.digits[offset+7]);
+    println!("{}", signal.get_8_digits(offset));
 }
 
 struct Signal {
@@ -33,30 +33,33 @@ impl Signal {
     fn apply_phase(&self) -> Signal {
         let mut digits = Vec::new();
         for n in 0..self.digits.len() {
-            digits.push(Signal::mult(&self.digits, &Signal::make_pattern(n, self.digits.len())));
+            digits.push(self.get_new_digit(n));
         }
         Signal { digits }
     }
 
-    fn make_pattern(n : usize, size : usize) -> Vec<i64> {
+    fn get_new_digit(&self, n : usize) -> i64 {
         let n1 = n + 1;
         let base : Vec<i64> = vec![0, 1, 0, -1];
-        let mut pattern = Vec::new();
-        for i1 in 1..=size {
-            pattern.push(base[(i1/n1) % 4]);
-        }
-        pattern
-    }
-
-    fn mult(v1 : &Vec<i64>, v2 : &Vec<i64>) -> i64 {
         let mut sum = 0;
-        for i in 0..v1.len() {
-            sum += v1[i] * v2[i];
+        let mut i = n;
+        while i < self.digits.len() {
+            let fact = base[((i + 1)/n1) % 4];
+            if fact == 0 {
+                i += n1;
+                continue;
+            }
+            sum += self.digits[i] * fact;
+            i += 1;
         }
         if sum > 0 {
             sum % 10
         } else {
             -sum % 10
         }
+    }
+
+    fn get_8_digits(&self, offset : usize) -> String {
+        format!("{}{}{}{}{}{}{}{}", self.digits[offset + 0], self.digits[offset + 1], self.digits[offset + 2], self.digits[offset + 3], self.digits[offset + 4], self.digits[offset + 5], self.digits[offset + 6], self.digits[offset + 7])
     }
 }
